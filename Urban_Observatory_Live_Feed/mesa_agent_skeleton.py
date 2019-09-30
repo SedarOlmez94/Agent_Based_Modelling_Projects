1aa3b4b35c889f259b59b7466fd319fdf858608c
SedarOlmez94 <solmez@turing.ac.uk>
2019-09-30 10:39:47 +0100

    MyModel class created (environment and time)
    MESA breaks up the conventional ABM craftsmanship into a model, agent, space and time.

--- Urban_Observatory_Live_Feed/mesa_agent_skeleton.py
+++ Urban_Observatory_Live_Feed/mesa_agent_skeleton.py
@@ -14,3 +14,20 @@ class MyAgent(Agent):
     def step(self):
         print("{} activated".format(self.name))
         # Agent actions when it is activated go within the step method.
+
+
+class MyModel(Model):
+    def __init__(self, n_agents):
+        super().__init__()
+        self.schedule = RandomActivation(self)
+        self.grid = MultiGrid(10, 10, torus = True)
+        for i in range(n_agents):
+            a = MyAgent(i, self)
+            self.schedule.add(a) # Add agent(s) to schedular
+            coords = (self.random.randrange(0, 10), self.random.randrange(0, 10))
+            # The coordinates which are randomly generated for each agent.
+            self.grid.place_agent(a, coords)
+
+
+    def step(self):
+        self.schedule.step()
