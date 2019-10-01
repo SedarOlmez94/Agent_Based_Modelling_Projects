55b51d7cfd1a8a9faa116797647dac970eb7e01d
SedarOlmez94 <solmez@turing.ac.uk>
2019-10-01 10:22:02 +0100

    A histogram of the wealth distribution for each agent after the run

--- Urban_Observatory_Live_Feed/Boltzmann_Wealth_Model_MESA/main.py
+++ Urban_Observatory_Live_Feed/Boltzmann_Wealth_Model_MESA/main.py
@@ -1,4 +1,10 @@
 from MoneyModel import MoneyModel
+import matplotlib.pyplot as plt
 
-empty_model = MoneyModel(10)
-empty_model.step()
+
+model = MoneyModel(10)
+for i in range(10):
+    model.step()
+agent_wealth = [a.wealth for a in model.schedule.agents]
+plt.hist(agent_wealth)
+plt.show()
