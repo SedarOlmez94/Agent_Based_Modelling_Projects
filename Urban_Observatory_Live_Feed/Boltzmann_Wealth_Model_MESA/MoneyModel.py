c0a5772f7bff36f4e72f58cb98d78a9d755f256f
SedarOlmez94 <solmez@turing.ac.uk>
2019-10-01 11:34:57 +0100

    give_money function written.
    This function finds all the agents in a given cell then adds one to their wealth and subtracts 1 from the wealth of the selected agent.

--- Urban_Observatory_Live_Feed/Boltzmann_Wealth_Model_MESA/MoneyModel.py
+++ Urban_Observatory_Live_Feed/Boltzmann_Wealth_Model_MESA/MoneyModel.py
@@ -4,10 +4,13 @@ from mesa.space import MultiGrid
 
 
 class MoneyAgent(Agent):
+
+
     """ An agent with fixed initial wealth """
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.wealth = 1
+
 
     def step(self):
         # The agent's step will go here. I.e. actions
@@ -20,7 +23,34 @@ class MoneyAgent(Agent):
         # Subtract 1 from the wealth of the current agent.
         self.wealth -= 1
 
+
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, # The first argument is the position of the moving agent
+            moore = True, # Moore set to true means the agent can move diagonal
+            include_center = False # Include the cell in which the agent is currently on.
+        )
+        # The new position is a randomly chosen position out of the possible_steps.
+        new_position = self.random.choice(possible_steps)
+        #
+        self.model.grid.move_agent(self, new_position)
+
+
+    def give_money(self):
+        #Get the agents that are in the same grid cell as the selected agent
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        # If the number of agents in the gridcell is more than 1 then
+        if len(cellmates) > 1:
+            # Select one of the other agents at random and add 1 to its wealth
+            other = self.random.choice(cellmates)
+            other.wealth += 1
+            # Subtract 1 from the selected agents wealth.
+            self.wealth -= 1
+
+
 class MoneyModel(Model):
+
+
     """ A model with some number of agents. """
     def __init__(self, N, width, height):
         self.num_agents = N
@@ -36,6 +66,7 @@ class MoneyModel(Model):
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
+
 
     def step(self):
         """ Advance the model by one step. """
