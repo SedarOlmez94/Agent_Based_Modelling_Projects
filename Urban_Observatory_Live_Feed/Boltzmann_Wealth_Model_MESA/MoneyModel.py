58ea6d0ac2e2638237be10ba281429efd075d239
SedarOlmez94 <solmez@turing.ac.uk>
2019-10-01 13:12:32 +0100

    The Gini Coefficient formula written in python in the compute_gini func.

--- Urban_Observatory_Live_Feed/Boltzmann_Wealth_Model_MESA/MoneyModel.py
+++ Urban_Observatory_Live_Feed/Boltzmann_Wealth_Model_MESA/MoneyModel.py
@@ -1,7 +1,16 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
 
+
+def compute_gini(model):
+    # The Gini Coefficient formula computed in python.
+    agent_wealths = [agent.wealth for agent in model.schedule.agents]
+    x = sorted(agent_wealths)
+    N = model.num_agents
+    B = sum( xi * (N-i) for i, xi in enumerate(x) ) / (N * sum(x))
+    return (1 + (1/N) - 2 * B)
 
 
 class MoneyAgent(Agent):
